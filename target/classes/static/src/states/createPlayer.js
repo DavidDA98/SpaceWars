e5b73f5d559872ff9e3cdbb84f86a0eb0f576720
sanmr <43404848+sanmr@users.noreply.github.com>
2019-05-03 11:03:27 +0200

    Player.name
    Asignar nombre a jugador (desde el teclado)

--- target/classes/static/src/states/createPlayer.js
+++ target/classes/static/src/states/createPlayer.js
@@ -1,7 +1,3 @@
-Spacewar.createPlayerState = function(game) {
-
-}
-
 Spacewar.createPlayerState.prototype = {
 
 	init : function() {
@@ -15,10 +11,37 @@ Spacewar.createPlayerState.prototype = {
 	},
 
 	create : function() {
-		game.state.start('chooseRoomState')
+		game.global.myPlayer.name = "";
+		game.input.keyboard.addCallbacks(this, null, null, keyPress);
+		backSpace = game.input.keyboard.addKey(Phaser.Keyboard.BACKSPACE);
+		text = game.add.text(50, 180, "User name: ", {
+			font: "65px Arial",
+			fill: "#ffff66"
+		});
+		next = game.add.button(480, 480, 'boton', botonNext, this, 1, 0);
+		
 	},
 
 	update : function() {
-
+		
+		if(backSpace.isUp && deletedChar == true){
+			deletedChar = false;
+		}
+		if (backSpace.isDown && deletedChar == false){
+			if (game.global.myPlayer.name != null && game.global.myPlayer.name.length > 0) {
+				game.global.myPlayer.name = game.global.myPlayer.name.substring(0, game.global.myPlayer.name.length - 1);
+				text.setText("User name: " +game.global.myPlayer.name);
+				
+			}
+			deletedChar = true;
+		}
 	}
+}
+function keyPress(char){
+	game.global.myPlayer.name += char;
+	text.setText("User name: " +game.global.myPlayer.name);
+}
+
+function botonNext(){
+	game.state.start('chooseRoomState')
 }
