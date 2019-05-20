efefc964a71e376916580867967031a153ba152f
JorgeDiegoA <43338580+JorgeDiegoA@users.noreply.github.com>
2019-05-20 21:52:58 +0200

    Se puede pasar a crear sala o a unirse a sala
    Se puede pasar al estado de crear sala o de unirse a sala dependiendo de lo que se seleccione. Se comienza a hacer el estado de creación de sala (sin terminar)

--- src/main/resources/static/src/states/newRoom.js
+++ src/main/resources/static/src/states/newRoom.js
@@ -11,14 +11,48 @@ Spacewar.newRoomState.prototype = {
 	},
 
 	preload : function() {
-
+		
 	},
 
 	create : function() {
-		game.state.start('matchmakingState')
+		roomName = "";
+		game.input.keyboard.addCallbacks(this, null, null, keyPress);
+		backSpace = game.input.keyboard.addKey(Phaser.Keyboard.BACKSPACE);
+		text = game.add.text(50, 180, "Room name: ", {
+			font: "65px Arial",
+			fill: "#ffff66"
+		});
+		game.add.button(480, 480, 'boton', botonNext, this, 1, 0);
 	},
 
 	update : function() {
+		if(backSpace.isUp && deletedChar == true){
+			deletedChar = false;
+		}
+		if (backSpace.isDown && deletedChar == false){
+			if (roomName != null && roomName.length > 0) {
+				rommName = roomName.substring(0, roomName.length - 1);
+				text.setText("User name: " + roomName);
+			}
+			deletedChar = true;
+		}
+	}
+}
 
+
+function keyPress(char){
+	roomName += char;
+	text.setText("User name: " + roomName);
+}
+
+
+function botonNext(){
+	if(roomName != null){
+		message = {
+				event : 'NEW ROOM'
+				room : roomName
+			}
+		game.global.socket.send(JSON.stringify(message))
+		game.state.start('matchmakingState')
 	}
 }
